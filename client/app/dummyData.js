e41fb55b95d8b10b3103faf4786fdb2d14cfc987
Brendan Dugan <dugan_brendan@yahoo.com>
2015-10-24 14:01:42 -0700

    [TEST] Add dummy data file

---
+++ client/app/dummyData.js
@@ -0,0 +1,2 @@
+var userData = {profImageUrl: "http://vignette3.wikia.nocookie.net/60seconds/images/0/02/Filepicker-cu8RtvHTyLM2781g44RQ_YOLO.jpg/revision/latest?cb=20150918035211",
+                currentItems: {}}
