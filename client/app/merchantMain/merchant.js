d818add2156b697b1ebbcf2bd8ef6e0ba07e4542
David Goldberg <drgold1988@gmail.com>
2015-10-24 22:25:52 -0700

    [Feature] updates merchant view with items that merchants save

--- client/app/merchantMain/merchant.js
+++ client/app/merchantMain/merchant.js
@@ -8,7 +8,14 @@ angular.module('myApp.merchant', ['ngRoute'])
     controller: 'MerchantCtrl'
   });
 }])
-
-.controller('MerchantCtrl', [function() {
-
+.controller('MerchantCtrl', ['$scope', function($scope) {
+  $scope.merchantData = {};
+  $scope.merchantData.profileImg = "http://thetshirtgame.com/neon_yolo_you_only_live_once_azalea_pink_bkgd.gif";
+  $scope.merchantData.merchantname = "Starbucks";
+  $scope.merchantData.merchantItems = [];
+  $scope.merchantData.merchantItems[0] = {itemid: 1, name: "Venti Mocha Latte", price: 4.99, description: "Large delicious latte from Starbucks"};
+  $scope.merchantData.merchantItems[1] = {itemid: 2, name: "Large Cheese Pizza", price: 9.99, description: "Delicious cheese pizza from Dominos"};
+  $scope.addMerchantItem = function(item) {
+    $scope.merchantData.merchantItems.push(item);
+  };
 }]);
