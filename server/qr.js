7cf21ccb44c0200ffabd40a4c4709eb799dab9ed
Ian Zhang <ianzhang1986@gmail.com>
2015-10-25 03:16:18 -0700

    [Cleanup] Remove dead code in qr.js

--- server/qr.js
+++ server/qr.js
@@ -2,7 +2,6 @@ var qr = require('qr-image');
 
 var createReceiptQR = function(encodeObj) {
   var png_string = qr.imageSync(JSON.stringify(encodeObj), { type: 'png', margin: 0});
-  // console.log(svg_string);
   return png_string;
 }
 
