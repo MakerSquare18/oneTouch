85155813d01ccff1a3298a01d2893c30bb2a3055
Ian Zhang <ianzhang1986@gmail.com>
2015-10-25 01:59:43 -0700

    [Refactor] Reduce white margins around qr_code

--- server/qr.js
+++ server/qr.js
@@ -1,7 +1,7 @@
 var qr = require('qr-image');
 
 var createReceiptQR = function(encodeObj) {
-  var png_string = qr.imageSync(JSON.stringify(encodeObj), { type: 'png' });
+  var png_string = qr.imageSync(JSON.stringify(encodeObj), { type: 'png', margin: 0});
   // console.log(svg_string);
   return png_string;
 }
