41f738a6650c8cf3b0d06c7dd6a42ca7a790e934
Ian Zhang <ianzhang1986@gmail.com>
2015-10-25 04:28:27 -0700

    [Update] Config merchant signup paypal sauce on server endpoint

--- server/routeHandlers/createMerchant.js
+++ server/routeHandlers/createMerchant.js
@@ -2,17 +2,30 @@ var db = require('../db');
 var paypal = require('../paypal');
 var Bluebird = require('bluebird');
 
+// Paypal sauce
+var CONFIG = require('../paypal/paypalConfig');
+
 module.exports = function createMerchant(req, res) {
   return new Promise(function(resolve, reject) {
     var newMerchantName = req.body.merchantName;
     var newMerchantInfo = req.body.merchantInfo;
-    var newMerchantClientId = req.body.merchantClientId;
-    var newMerchantSecret = req.body.merchantSecret;
+    // var newMerchantClientId = req.body.merchantClientId;
+    // var newMerchantSecret = req.body.merchantSecret;
+    // var newMerchant = {
+    //   merchantName: newMerchantName,
+    //   info: newMerchantInfo,
+    //   items: []
+    // };
+
     var newMerchant = {
-      merchantName: newMerchantName,
-      info: newMerchantInfo,
+      merchantName: req.body.merchantName,
+      password: req.body.password,
+      info: req.body.merchantInfo || {},
       items: []
     };
+
+    var newMerchantClientId = CONFIG.CLIENT_ID;
+    var newMerchantSecret = CONFIG.SECRET;
     db.createMerchant(newMerchant);
     paypal.authenticateServer(newMerchantClientId, newMerchantSecret)
     .then(function(merchantTokens) {
