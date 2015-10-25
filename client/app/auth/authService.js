457e524316f720b8b182091d02f0bef76e85239d
Brendan Dugan <dugan_brendan@yahoo.com>
2015-10-25 03:14:13 -0700

    [FIX] removed auth services

--- client/app/auth/authService.js
+++
@@ -1,7 +0,0 @@
-angular.module('myApp.auth')
-.factory("AuthFactory", [function(){
-  var AuthFactory = {};
-  //TODO: make this an actual thing and not some nonsense
-  AuthFactory.username = "makersquare18";
-  return AuthFactory;
-}]);
